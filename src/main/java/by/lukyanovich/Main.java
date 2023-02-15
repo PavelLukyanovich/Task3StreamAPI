34ec50ca0f8233b345a75701673d1aac54e78cd5
Pavel Lukyanovich <paellukyanovich1@gmail.com>
2023-02-15 17:22:26 +0300

    style: clean code

--- src/main/java/by/lukyanovich/Main.java
+++ src/main/java/by/lukyanovich/Main.java
@@ -10,27 +10,25 @@ import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
 import static by.lukyanovich.model.Days.FRIDAY;
 import static by.lukyanovich.model.Days.MONDAY;
-
 public class Main {
     public static void main(String[] args) throws IOException {
-//        task1();
-//        task2();
-//        task3();
-//        task4();
-//        task5();
-//        task6();
-//        task7();
-//        task8();
-//        task9();
-//        task10();
-//        task11();
-//        task12();
-//        task13();
-//        task14();
-//        task15();
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+        task9();
+        task10();
+        task11();
+        task12();
+        task13();
+        task14();
+        task15();
         task16();
     }
 
